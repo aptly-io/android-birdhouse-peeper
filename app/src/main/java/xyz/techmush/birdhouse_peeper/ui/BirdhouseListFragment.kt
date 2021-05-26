59aba1ac87a9a095e608341897c1e60cb7b215bc
F M <f@techmush.xyz>
2021-05-26 22:09:43 +0200

    Add image in list fragment

--- app/src/main/java/xyz/techmush/birdhouse_peeper/ui/BirdhouseListFragment.kt
+++ app/src/main/java/xyz/techmush/birdhouse_peeper/ui/BirdhouseListFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.doOnLayout
 import androidx.databinding.BindingAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -20,6 +21,7 @@ import xyz.techmush.birdhouse_peeper.databinding.BirdhouseListFragmentBinding
 import xyz.techmush.birdhouse_peeper.databinding.ViewBirdhouseBinding
 import xyz.techmush.birdhouse_peeper.model.BirdhouseRepository
 import xyz.techmush.birdhouse_peeper.model.BirdhouseRepository.Birdhouse
+import xyz.techmush.birdhouse_peeper.util.setImageFromPath
 import xyz.techmush.birdhouse_peeper.vm.BirdhouseListViewModel
 import javax.inject.Inject
 
@@ -96,6 +98,10 @@ class BirdhouseListAdapter(private val viewModel: BirdhouseListViewModel):
             binding.birdhouse = birdhouse
             binding.vm = viewModel
             binding.executePendingBindings()
+
+            binding.imageView.doOnLayout {
+                setImageFromPath(binding.imageView, birdhouse.photo)
+            }
 
             if (viewModel.birdhouse.value?.address == birdhouse.address) {
                 binding.root.setBackgroundResource(R.color.purple_200)
