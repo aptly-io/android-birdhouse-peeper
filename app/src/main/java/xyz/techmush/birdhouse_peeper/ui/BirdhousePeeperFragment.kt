4624910daf8b5281feec7eb037a966e682f657b8
F M <f@techmush.xyz>
2021-05-28 15:17:41 +0200

    Fix RecyclerView item's size

--- app/src/main/java/xyz/techmush/birdhouse_peeper/ui/BirdhousePeeperFragment.kt
+++ app/src/main/java/xyz/techmush/birdhouse_peeper/ui/BirdhousePeeperFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
@@ -43,9 +45,37 @@ class BirdhousePeeperFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         viewModel.event.observe(viewLifecycleOwner, { event -> when (event) {
-            is BirdhousePeeperViewModel.Event.Navigate -> {
+
+            is BirdhousePeeperViewModel.Event.LoadUrl -> {
+                // todo hack to confirm the URLs got really processed
                 Timber.d("URL: ${event.url}")
-                webView.loadUrl(event.url)
+
+                val urls = listOf(
+                    "http://192.168.40.44:80/control?var=quality&val=4",
+                    "http://192.168.40.44:80/control?var=framesize&val=9",
+                    "http://192.168.40.44:80/control?var=led_intensity&val=49",
+                    event.url)
+                val numbersIterator = urls.iterator()
+
+                webView.webViewClient = object: WebViewClient() {
+                    override fun onPageFinished(view: WebView?, url: String?) {
+                        super.onPageFinished(view, url)
+
+                        if (numbersIterator.hasNext()) {
+                            val url = numbersIterator.next()
+                            Timber.d("Loading next: $url")
+                            webView.loadUrl(url)
+                        } else {
+                            Timber.d("Loading done")
+                        }
+                    }
+                }
+                webView.loadUrl(numbersIterator.next())
+            }
+            is BirdhousePeeperViewModel.Event.NavigateEdit -> {
+                findNavController().navigate(BirdhousePeeperFragmentDirections
+                    .actionBirdhousePeeperFragmentToBirdhouseConfigFragment(
+                        viewModel.birdhouse.value!!.address, ""))
             }
         } })
     }
